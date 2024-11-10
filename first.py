d0d21a3337d99259a14d696644f7168a6332e39e
umanskky <umanskky@gmail.com>
2024-11-10 20:08:11 +0300

    Работа с файлами, упаковка и раскаковка

--- first.py
+++ first.py
@@ -1,16 +1,52 @@
-f = open("data.txt", "w")
-f.write("Hallo\n")
-f.write("World\n")
+import pickle, shelve
+
+"""
+print("Консервация списков")
+
+variety = ["огурцы", "помидоры", "капуста"]
+shape   = ["целые", "кубиком", "соломкой"]
+brand   = ["Главпродукт", "Чумак", "Бондюэль"]
+
+f = open("pickles.dat", "wb")
+
+pickle.dump(variety, f)
+pickle.dump(shape, f)
+pickle.dump(brand, f)
+
 f.close()
-f = open("data.txt", "+a")
-f.write("Hallo\n")
-f.write("World\n")
+
+
+print("Расконсервация списков")
+
+f = open("pickles.dat", "rb")
+
+variety = pickle.load(f)
+shape = pickle.load(f)
+brand = pickle.load(f)
+
+print(variety)
+print(shape)
+print(brand)
+
 f.close()
-f = open("data.txt", "+a")
-f.write("Alex\n")
-f.write("Umanskky\n")
-f.close()
-f = open("data.txt", "+a")
-f.write("Egor\n")
-f.write("Umanskky\n")
-f.close()
+
+"""
+
+print("Помещение списков на полку")
+
+s = shelve.open("picles2.dat")
+
+s["variety"] = ["огурцы", "помидоры", "капуста"]
+s["shape"]   = ["целые", "кубиком", "соломкой"]
+s["brand"]   = ["Главпродукт", "Чумак", "Бондюэль"]
+
+s.sync()
+
+print("Извлечение списков из файла полки:")
+
+print("торговые  марки - ", s["brand"])
+print("формы - ", s["shape"])
+print("виды овощей - ", s["variety"])
+
+s.close()
+
