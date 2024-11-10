1325c61d2dea8fcf0b1df554cf31f679e9dfbc2a
umanskky <umanskky@gmail.com>
2024-11-10 22:44:05 +0300

    ООП - введение

--- first.py
+++ first.py
@@ -1,52 +1,29 @@
-import pickle, shelve
+# ООП
 
-"""
-print("Консервация списков")
+class Critter(object):
 
-variety = ["огурцы", "помидоры", "капуста"]
-shape   = ["целые", "кубиком", "соломкой"]
-brand   = ["Главпродукт", "Чумак", "Бондюэль"]
+    def __init__(self, name):
+        print("Появился новый зверь")
+        self.name = name
 
-f = open("pickles.dat", "wb")
+    def __str__(self):
+        rep = "Объект класса Critten\n"
+        rep += "имя: " + self.name + "\n"
+        return rep
 
-pickle.dump(variety, f)
-pickle.dump(shape, f)
-pickle.dump(brand, f)
+    def talk(self):
+        print("Привет. Меня зовут", self.name, "\n")
 
-f.close()
+crit1 = Critter("Бобик")
+crit1.talk()
+crit2 = Critter("Мурзик")
+crit2.talk()
+
+print("Вывод объекта Crit1 на экран:")
+print(crit1)
+print("Непостредственный доступ к атрибуту crit1.name:")
+print(crit1.name)
 
 
-print("Расконсервация списков")
 
-f = open("pickles.dat", "rb")
-
-variety = pickle.load(f)
-shape = pickle.load(f)
-brand = pickle.load(f)
-
-print(variety)
-print(shape)
-print(brand)
-
-f.close()
-
-"""
-
-print("Помещение списков на полку")
-
-s = shelve.open("picles2.dat")
-
-s["variety"] = ["огурцы", "помидоры", "капуста"]
-s["shape"]   = ["целые", "кубиком", "соломкой"]
-s["brand"]   = ["Главпродукт", "Чумак", "Бондюэль"]
-
-s.sync()
-
-print("Извлечение списков из файла полки:")
-
-print("торговые  марки - ", s["brand"])
-print("формы - ", s["shape"])
-print("виды овощей - ", s["variety"])
-
-s.close()
 
