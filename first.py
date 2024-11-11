9f8ef285e9ad17405901ae43513eff67fbd983a3
umanskky <umanskky@gmail.com>
2024-11-11 15:28:15 +0300

    Принципы ООП (Инкапс. Наслед. Полиформ. Абстр.)

--- first.py
+++ first.py
@@ -1,29 +1,115 @@
 # ООП
+""""
+# ИНКАПСУЛЯЦИЯ
+class Cat():
 
-class Critter(object):
+    def __init__(self, breed, color, age):
+        self._breed = breed
+        self._color = color
+        self._age   = age
 
-    def __init__(self, name):
-        print("Появился новый зверь")
-        self.name = name
+    @property
+    def breed(self):
+        return self._breed
+    
+    @property
+    def color(self):
+        return self._color
 
-    def __str__(self):
-        rep = "Объект класса Critten\n"
-        rep += "имя: " + self.name + "\n"
-        return rep
+    @property
+    def age(self):
+        return self._age
 
-    def talk(self):
-        print("Привет. Меня зовут", self.name, "\n")
+    @age.setter
+    def age(self, new_age):
+        if new_age > self._age:
+            self._age = new_age
+        return self._age
 
-crit1 = Critter("Бобик")
-crit1.talk()
-crit2 = Critter("Мурзик")
-crit2.talk()
+    def meow(self):
+        print("Мяу!")
 
-print("Вывод объекта Crit1 на экран:")
-print(crit1)
-print("Непостредственный доступ к атрибуту crit1.name:")
-print(crit1.name)
+    def purr(self):
+        print("Мрррр")
+
+#Создаем новый класс --- НАСЛЕДОВАНИЕ
+
+class HomeCat(Cat):
+    
+    def __init__(self, breed, color, age, owner, name):
+        super().__init__(breed, color, age)
+        self._owner = owner
+        self._name = name
+
+    @property
+    def owner(self):
+        return self._owner
+    
+    @property
+    def name(self):
+        return self._name
+
+    def getTreat(self):
+        print("мяу-мяу")
+
+my_cat = HomeCat("Сиамская", "Белая", 3, "Иван", "Роза")
+
+print(my_cat.owner)
+print(my_cat.breed)
+print(my_cat.color)
+my_cat.getTreat()
+my_cat.purr()
+
+cat1 = Cat("Абиссинская", "Рыжая", 4)
+
+cat1.meow()
+print(cat1.color)
+print(cat1.breed)
+cat1.age = 12
+print(cat1.age)
 
 
+# --- ПОЛИФОРМИЗМ
+class Cat:
+    
+    def sleep(self):
+        print('Свернулся в клубок и сладко спит.')
 
+class Parrot:
+    
+    def sleep(self):
+        print('Сел на жёрдочку и уснул.')
+
+def homeSleep(animal):
+    animal.sleep()
+
+cat = Cat()
+parrot = Parrot()
+homeSleep(cat) # Свернулся в клубок и сладко спит.
+homeSleep(parrot) # Сел на жёрдочку и уснул.
+
+"""
+# --- АБСТРАКЦИЯ
+class Predator:
+    
+    def hunt(self):
+        print('Охотится...')
+
+class Cat(Predator):
+    
+    def __init__(self, name, color):
+        super().__init__()
+        self._name = name
+        self._color = color
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def color(self):
+        return self._color
+
+cat = Cat('Даниэла', 'Чёрный')
+cat.hunt() # Охотится…
 
